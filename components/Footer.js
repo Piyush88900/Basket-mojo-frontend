8f327ad55234e0c127ca9b4bf264adc22c2d0f24
Piyush88900 <piyushsah396@gmail.com>
2025-08-01 18:09:52 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => (
+  <footer style={{
+    backgroundColor: 'var(--primary-color)',
+    color: 'var(--secondary-color)',
+    padding: '10px',
+    marginTop: '20px',
+    textAlign: 'center'
+  }}>
+    <p style={{ margin: 0 }}>&copy; 2024 Basket Mojo</p>
+  </footer>
+);
+
+export default Footer;
